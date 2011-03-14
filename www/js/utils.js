4560735fcc6dd9b5cead327e970f9c8b727e035c
Eric Mill <kprojection@gmail.com>
2011-03-14 18:54:31 -0400

    Some halting progress at getting inter-page communication and traversal going

--- www/js/utils.js
+++ www/js/utils.js
@@ -4,5 +4,23 @@ Utils = {
   // adb logcat PhoneGapLog:V *:S
   log: function(msg) {
     console.log("\n\n" + msg);
+  },
+  
+  // depends on jQuery and JQM being loaded, will run a function on page load
+  show: function (selector, callback) {
+    $(selector).live("pageshow", callback);
+  },
+  
+  // adapted from http://stackoverflow.com/questions/901115/get-querystring-values-with-jquery
+  param: function (name) {
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec( window.location.href );
+    if( results == null )
+      return "";
+    else
+      return decodeURIComponent(results[1].replace(/\+/g, " "));
   }
+
 };
