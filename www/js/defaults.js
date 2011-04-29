f87d5bf8560bf58493cc13858f48ead1f12e1f48
Eric Mill <kprojection@gmail.com>
2011-04-29 18:08:16 -0400

    Hide the fact that the user is clicking on an external link

--- www/js/defaults.js
+++ www/js/defaults.js
@@ -2,3 +2,9 @@ $(document).bind("mobileinit", function(){
   $.mobile.defaultTransition = 'none';
   //$.mobile.loadingMessage = false;
 });
+
+// hide the fact that the user is clicking on an external link
+$("#stickey_footer div ul li a[rel=external]").live("click", function() {
+  $.mobile.pageLoading();
+  return true;
+});
