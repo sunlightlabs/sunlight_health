c1d81c1a8af50d3f28b5781d26d431fd55d78b71
Eric Mill <kprojection@gmail.com>
2011-07-01 12:08:06 -0400

    New phonegap 0.9.6 based project

---
+++ ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  health_care_2
+//
+//  Created by Development on 7/1/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
